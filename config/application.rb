db909451aba37a89b29a277c3d67354603624e42
Jeremy ISRAEL <israel.jeremy@gmail.com>
2016-12-26 10:29:15 +0100

    navbar under construction

--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,13 @@ Bundler.require(*Rails.groups)
 
 module NexityTerrains
   class Application < Rails::Application
+
+
     config.generators do |generate|
       generate.assets false
     end
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
